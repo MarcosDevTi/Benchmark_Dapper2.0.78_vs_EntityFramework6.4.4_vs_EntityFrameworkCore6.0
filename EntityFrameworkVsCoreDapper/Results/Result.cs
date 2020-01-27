28a672d66e3704515ea1f0fea6033a0a89d1f4a0
Marcos Souza <marcos.dev.ti@gmail.com>
2020-01-27 00:28:22 -0500

    Results Entities

---
+++ EntityFrameworkVsCoreDapper/Results/Result.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkVsCoreDapper.Results
+{
+    public class Result : Entity
+    {
+        public DateTime Date { get; set; }
+        public ICollection<ResultItem> ResultItems { get; set; }
+    }
+}
---
+++ EntityFrameworkVsCoreDapper/Results/ResultItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityFrameworkVsCoreDapper.Results
+{
+    public class ResultItem : Entity
+    {
+        public TimeSpan Dapper { get; set; }
+        public TimeSpan Ef6 { get; set; }
+        public TimeSpan EfCore { get; set; }
+        public TimeSpan EfCoreAsNoTracking { get; set; }
+        public (string Name, TimeSpan Speed) Faster()
+        {
+            var arr = new (string Name, TimeSpan Tempo)[]
+            {
+                ("Dapper", Dapper),("Ef 6", Ef6), ("Ef Core", EfCore), ("Ef Core AsNoTracking", EfCoreAsNoTracking)
+            };
+            return arr.FirstOrDefault(_ => _.Tempo.TotalSeconds == arr.Min(_ => _.Tempo).TotalSeconds);
+        }
+    }
+}
